dcfdd4655813998d8f8e7cbafa981fb80cd39a7c
lukamamgu <lukamanguashvili@gmail.com>
2025-03-26 17:54:57 +0400

    commit

---
+++ day 012/classwork/main.py
@@ -0,0 +1,6 @@
+print(5 < 7)#true
+print(8 > 6)#true
+print(10 == 10)#true
+print(10 <= 11)#true
+print(7 >= 6)#true
+print(3 != 4)#true
---
+++ day 012/classwork/main1.py
@@ -0,0 +1,14 @@
+#not
+print(not False)#true
+print(not True)#false
+
+#and
+print(True and False)#false
+print(False and True)#false
+print(False and False)#false
+
+#or
+print(True and True)#true
+print(False and True)#true
+print(True and False)#true
+print(False and False)#false
