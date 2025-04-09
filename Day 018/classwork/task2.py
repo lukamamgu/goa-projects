9e4fb552ef3c7b33628393a54d8aad8084555b40
lukamamgu <lukamanguashvili@gmail.com>
2025-04-09 13:49:29 +0400

    commit

---
+++ Day 018/classwork/task2.py
@@ -0,0 +1,5 @@
+score = int(input("enter your score: "))
+if score >= 50:
+    print("you passed")
+if score < 50 :
+    print("you failed")
---
+++ Day 018/homework/home3.py
@@ -0,0 +1,5 @@
+password = "goabest123"
+user_entery =""
+while user_entery != password:
+    user_entery = input("enter the password: ")
+    print("password is correct")
