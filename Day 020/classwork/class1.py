af93012a2d3888422fa3117bfb9964f9e6de0537
lukamamgu <lukamanguashvili@gmail.com>
2025-04-23 18:32:14 +0400

    commit

---
+++ Day 020/classwork/class1.py
@@ -0,0 +1,10 @@
+num = int(input("Enter a number: "))
+if num > 0 :
+    print("Positive number")
+if num % 2 == 0:
+    print("Even number")
+else :
+    print("Odd number")
+
+if num < 0:
+     print("Negative number")
